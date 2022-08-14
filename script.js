723e67977bd11e2eefd29f530642f824b908a461
Anshul Papneja <anshulpapneja@outlook.com>
2022-08-14 17:10:55 -0400

    Add functionality to change grid size

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-const grid = document.getElementById('grid');
-const numOfBoxesInRow = 16;
-const boxSideLength = grid.clientHeight / numOfBoxesInRow;      //in pixels
+function createGrid(numOfBoxesInRow) {
+  grid.innerHTML = '';
 
-//creates the grid
-for (let i = 0; i < numOfBoxesInRow; i++) {
-  for (let j = 0; j < numOfBoxesInRow; j++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    box.setAttribute('id', `${i},${j}`);
-    box.style.width = `${boxSideLength}px`;
-    box.style.height = `${boxSideLength}px`;
-    grid.appendChild(box);
+  let boxSideLength = grid.clientWidth / numOfBoxesInRow;
+
+  for (let i = 0; i < numOfBoxesInRow; i++) {
+    for (let j = 0; j < numOfBoxesInRow; j++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      box.setAttribute('id', `${i},${j}`);
+      box.style.width = `${boxSideLength}px`;
+      box.style.height = `${boxSideLength}px`;
+      grid.appendChild(box);
+    }
   }
+
+  addHoverEvent();
 }
-
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach(box => box.addEventListener('pointerenter', changeBoxColor));
 
 function changeBoxColor(e) {
   e.target.style.backgroundColor = 'yellow';
 }
+
+function changeGridSize() {
+  numOfBoxesInRow = Number(prompt('Enter the number of boxes per side for the new grid (max. 100).'));
+  if (!Number.isInteger(numOfBoxesInRow) || numOfBoxesInRow <= 0 || numOfBoxesInRow > 100) {
+    alert('Input was not an integer between 1 and 100. Grid size not changed.');
+    return;
+  }
+
+  createGrid(numOfBoxesInRow);
+}
+
+function addHoverEvent() {
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach(box => box.addEventListener('pointerenter', changeBoxColor));
+}
+
+const grid = document.getElementById('grid');
+const changeGridSizeBtn = document.getElementById('change-grid-size');
+
+changeGridSizeBtn.addEventListener('click', changeGridSize);
+
+createGrid(16);
